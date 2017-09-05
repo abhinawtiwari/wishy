d61c3c39292a5372a242df4ce32da9b5b28db381
panch mukesh <mukesh.panch14@gmail.com>
2017-09-06 01:53:57 +0530

    app.js changes 5th sept

--- nodekb/app.js
+++ nodekb/app.js
@@ -1,7 +1,10 @@
 const express= require('express');
 const path=require('path');
 const mongoose=require('mongoose');
-const bodyParser=require('body-parser')
+const bodyParser=require('body-parser');
+const expressValidator=require('express-validator');
+const flash=require('connect-flash');
+const session=require('express-session');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db=mongoose.connection;
@@ -35,6 +38,40 @@ app.use(bodyParser.json());
 
 //Set Public Folder
 app.use(express.static(path.join(__dirname,'public')));
+
+//Express Session Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie:(secure:true)
+}));
+
+//Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter:function(param,msg,value){
+    var namespace=param.split('.'),
+    root= namespace.shift(),
+    formParam=root;
+
+    while(namespace.length){
+      formParam+='['+namespace.shift()+']';
+    }
+    return{
+      param:formParam,
+      msg=msg,
+      value=value
+    };
+
+  }
+}));
 
 //Home Route
 
